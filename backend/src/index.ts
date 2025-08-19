0af4f47ae70a96f93f9904bb57054e1805deb8ac
Om Pethani <119727186+ompethani07@users.noreply.github.com>
2025-08-19 16:46:58 +0530

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,7 +9,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(express.json());
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true }));
 app.use("/auth", authRouter);
 app.use("/task", taskRouter);
 
